319b05b4a1941608ac97c2441bb55581cdbf0b7f
MH <150673972+mayank-hassija@users.noreply.github.com>
2025-04-28 22:45:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
-  },
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: ['frontend-dvwq.onrender.com']
+  }
 });
