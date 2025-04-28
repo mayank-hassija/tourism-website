56e42e552af5c8df39d6ca2334d3eec6ecf814e2
MH <150673972+mayank-hassija@users.noreply.github.com>
2025-04-29 01:02:32 +0530

    Update Tour.js

--- backend/models/Tour.js
+++ backend/models/Tour.js
@@ -1,17 +1,12 @@
 import mongoose from 'mongoose';
 
 const tourSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  description: String,
-  price: {
-    type: Number,
-    required: true
-  }
+  title: { type: String, required: true },
+  description: { type: String },
+  location: { type: String },
+  price: { type: Number },
+  image: { type: String } // <-- added this line
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
-
 export default Tour;
