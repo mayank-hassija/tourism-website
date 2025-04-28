88e748092ebb874b7d5648b80fa687ebb4e165b2
MAYANK HASSIJA <mayankhassija@gmail.com>
2025-04-28 23:33:12 +0530

    small bug

--- backend/server.js
+++ backend/server.js
@@ -28,4 +28,5 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
