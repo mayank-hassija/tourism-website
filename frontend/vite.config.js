d1d849d47572e111ca087d779e8034a51d63cee3
MH <150673972+mayank-hassija@users.noreply.github.com>
2025-04-29 19:28:37 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env,
+  },
   preview: {
     host: '0.0.0.0',
     port: 10000,
