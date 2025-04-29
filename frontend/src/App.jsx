ac65a8112d67514bab110babbaf4940cc74be5cc
MH <150673972+mayank-hassija@users.noreply.github.com>
2025-04-29 17:25:33 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+// frontend/src/App.jsx
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ToursPage from "./pages/ToursPage";
 import TourDetailsPage from "./pages/TourDetailsPage";
@@ -8,13 +10,19 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      {/* No Navbar here */}
-
+      {/* Main App Container with page routing */}
       <div style={{ padding: "20px", paddingTop: "20px" }}>
         <Routes>
+          {/* Home - list of all tours */}
           <Route path="/" element={<ToursPage />} />
+
+          {/* View a specific tour's details */}
           <Route path="/tour/:id" element={<TourDetailsPage />} />
+
+          {/* Add a new tour */}
           <Route path="/add-tour" element={<AddTourPage />} />
+
+          {/* Edit an existing tour */}
           <Route path="/edit-tour/:id" element={<EditTourPage />} />
         </Routes>
       </div>
